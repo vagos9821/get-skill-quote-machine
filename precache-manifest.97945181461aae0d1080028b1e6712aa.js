2f69ff53ddb7270d09d18636f97c96e63abff73a
Vangelis Sigalas <yourtube12321@gmail.com>
2024-02-16 08:30:19 +0200

    Updates

---
+++ precache-manifest.97945181461aae0d1080028b1e6712aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3acedaa0d27a45d9596705fb03d0f0c",
+    "url": "/vagos9821/get-skill-quote-machine/index.html"
+  },
+  {
+    "revision": "ed584906d3e055e42f9e",
+    "url": "/vagos9821/get-skill-quote-machine/static/css/main.0dee8d6b.chunk.css"
+  },
+  {
+    "revision": "57c5b052797a5c6aa26a",
+    "url": "/vagos9821/get-skill-quote-machine/static/js/2.a34c9e5a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/vagos9821/get-skill-quote-machine/static/js/2.a34c9e5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785b3f8f066c6f39952d",
+    "url": "/vagos9821/get-skill-quote-machine/static/js/3.71a2354e.chunk.js"
+  },
+  {
+    "revision": "ed584906d3e055e42f9e",
+    "url": "/vagos9821/get-skill-quote-machine/static/js/main.c5369110.chunk.js"
+  },
+  {
+    "revision": "9eae33aac821fb32fa48",
+    "url": "/vagos9821/get-skill-quote-machine/static/js/runtime-main.f550ecbc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vagos9821/get-skill-quote-machine/precache-manifest.97945181461aae0d1080028b1e6712aa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vagos9821/get-skill-quote-machine/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
